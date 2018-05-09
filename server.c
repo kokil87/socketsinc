459c08d0e0999136014caf084998b0ab07406dc8
kokil87 <piyushsethia1999@gmail.com>
2018-05-09 14:44:17 +0530

    Improvements

--- server.c
+++ server.c
@@ -13,32 +13,18 @@
 #define BACKLOG 5
 #define MAXIMUM_SIZE 100
 #define FLAG 0
+#define PORT "8080"
 
-int max(int a, int b){
-	return (a>b)?a:b;
-}
+struct timeval timeIsPrecious;
+size_t len;
+char msg_to_be_sent[] = "HTTP/1.0 200 OK\nContent-Type: text/html; charset=UTF-8\nContent-Length:15 \n\n<html>ll</html>";
 
-char* getIp(struct sockaddr_storage client_addr) {
-    if(client_addr.ss_family == AF_INET) {
-		struct sockaddr_in *inet = (struct sockaddr_in *)&client_addr;
-		char ip[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, &(inet->sin_addr), ip, INET_ADDRSTRLEN);
-		char *ip4 = (char *)malloc(strlen(ip)+1);
-	    strcpy(ip4,ip);
-	    return ip4;
-	}
-	else {
-		struct sockaddr_in6 *inet6 = (struct sockaddr_in6 *)&client_addr;
-		char ip[INET6_ADDRSTRLEN];
-		inet_ntop(AF_INET6, &(inet6->sin6_addr), ip, INET_ADDRSTRLEN);
-		char *ip6 = (char *)malloc(strlen(ip)+1);
-	    strcpy(ip6,ip);
-	    return ip6;
-	}
-}
-
-void fixmsg(char **msg) {
-	*msg = "HTTP....";
+char* getIp(struct sockaddr_in *client_addr) {
+	char ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(client_addr->sin_addr), ip, INET_ADDRSTRLEN);
+	char *ip4 = (char *)malloc(strlen(ip)+1);
+    strcpy(ip4,ip);
+    return ip4;
 }
 
 void sendError(char *p) {
@@ -46,41 +32,27 @@ void sendError(char *p) {
 	perror("");
 }
 
-int main(int argc, char const *argv[])
-{
-	int status, socketfd, total_msg_sent, bytes_recv, bytes_sent, yes = 1;
-	size_t len;
-	
-	socklen_t addr_size;
-	
-	char *msg;
-	char *msg_to_be_sent = (char*)malloc(100);
-	fixmsg(&msg_to_be_sent);
-	printf("%s\n", msg_to_be_sent);
-	len = sizeof msg_to_be_sent;
+int getSocketForBinding(){
+	int status, socketfd, yes = 1;
 
 	struct addrinfo hints;
-	struct sockaddr_storage client_addr;
 	struct addrinfo *servres;
-	fd_set all;
-	fd_set currently_all;
-	struct timeval timeIsPrecious;
-	timeIsPrecious.tv_usec = 1000;
-
+	struct addrinfo *serverInfo;
+	
 	memset(&hints, 0, sizeof hints);
 
 	hints.ai_family = AF_INET;
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
 
-	status = getaddrinfo( NULL, "8080", &hints, &servres );
+	status = getaddrinfo( NULL, PORT, &hints, &servres );
 
 	if(status) {
 		perror("No IP found");
 		exit(1);
 	}
 
-	struct addrinfo *serverInfo = servres;
+	serverInfo = servres;
 
 	for (; serverInfo != NULL; serverInfo = serverInfo->ai_next)
 	{
@@ -103,50 +75,60 @@ int main(int argc, char const *argv[])
 		printf("Sorry No server available\n");
 		exit(1);
 	}
+	freeaddrinfo(servres);
 	if(listen(socketfd, BACKLOG) == -1) {sendError("listening"); exit(1); }
-	FD_SET(socketfd, &(all));
-	int fdmax = socketfd;
+	return socketfd;	
+}
+
+void handleClient(int new_fd) {
+	
+	int total_msg_sent = 0;
+
+	while (total_msg_sent < len) {
+		int p = send(new_fd, msg_to_be_sent, len, FLAG);
+		total_msg_sent += p;
+	}
+
+	close(new_fd);
+}
+
+int main(int argc, char const *argv[])
+{
+	int listener, bytes_recv, clientfd;
+	socklen_t addr_size;
+	struct sockaddr_storage client_addr;
+
+	timeIsPrecious.tv_usec = 1000;
+
+	char *msg;
+
+	len = sizeof msg_to_be_sent;
+
+
+	fd_set all;
+	fd_set currently_all;
+
+	listener = getSocketForBinding();
+
+	FD_SET(listener, &(all));
+	int fdmax = listener;
+
 	while(1) {
+
 		currently_all = all;
-		//SERVER WILL SEND DATA WHEN IT RECV SOMETHING OR IT FINDS A NEW CONNECTION
+
 		int res = select(fdmax + 1, &currently_all, NULL, NULL, &timeIsPrecious);
 		for (int i = 0; i <= fdmax; ++i)
 		{
 			if(FD_ISSET(i, &currently_all)) {
-				if(i == socketfd) {
+				if(i == listener) {
+					int s = (int)len;
 					addr_size = sizeof client_addr;
-					int new_fd = accept(socketfd, (struct sockaddr *)&client_addr, &addr_size);
+					int new_fd = accept(listener, (struct sockaddr *)&client_addr, &addr_size);
 					if(new_fd == -1) { sendError("new_fd"); continue; }
-					FD_SET(new_fd, &all);
-					fdmax = max(fdmax, new_fd);
-					printf("The following connection has been established:\t%s\n", getIp(client_addr));
-					total_msg_sent = 0;
-
-					while (total_msg_sent < len) {
-						printf("In here %d :: %s\n", i, msg_to_be_sent);
-						int p = send(new_fd, msg_to_be_sent, len, FLAG);
-						if (p == -1)
-							printf("Got an error while sending\n");
-						total_msg_sent += p;
-					}
-					printf("In here again\n");
-				}
-				else {
-					if((bytes_recv = recv(i, msg, MAXIMUM_SIZE, FLAG)) == 0) {
-						if(i == fdmax) fdmax--;
-						FD_CLR(i, &all);
-						printf("One Connection Lost\n");
-						close(i);
-					}
-					else if(bytes_recv == -1) {
-						sendError("Receiving");
-					}
 					else {
-						printf("%s\n", msg);
-						total_msg_sent = 0;
-						while (total_msg_sent < len) {
-							total_msg_sent += send(i, msg, len, FLAG);
-						}
+						printf("The following connection has been established:\t%s\n", getIp((struct sockaddr_in*)&client_addr));
+						handleClient(new_fd);
 					}
 				}
 			}
